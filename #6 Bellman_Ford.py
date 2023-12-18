2cc0db7e694343b1d6063fab5fd73c406a33c494
M O H I T  K U M A R <141761768+mhtkr@users.noreply.github.com>
2023-12-18 20:41:50 +0530

    Create #6 Bellman_Ford.py

---
+++ #6 Bellman_Ford.py
@@ -0,0 +1,44 @@
+import time
+
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = []
+
+    def add_edge(self, s, d, w):
+        self.graph.append([s, d, w])
+
+    def print_solution(self, dist):
+        print("Vertex Distance from Source")
+        for i in range(self.V):
+            print("{0}\t\t{1}".format(i, dist[i]))
+
+    def bellman_ford(self, src):
+        dist = [float("inf")] * self.V
+        dist[src] = 0
+
+        for _ in range(self.V - 1):
+            for s, d, w in self.graph:
+                if dist[s] != float("inf") and dist[s] + w < dist[d]:
+                    dist[d] = dist[s] + w
+
+        for s, d, w in self.graph:
+            if dist[s] != float("inf") and dist[s] + w < dist[d]:
+                print("Graph contains a negative weight cycle")
+                return
+
+        self.print_solution(dist)
+
+g = Graph(5)
+g.add_edge(0, 1, 5)
+g.add_edge(0, 2, 4)
+g.add_edge(1, 3, 3)
+g.add_edge(2, 1, 6)
+g.add_edge(3, 2, 2)
+
+start_time = time.time()
+g.bellman_ford(0)
+end_time = time.time()
+execution_time = (end_time - start_time) * 1000
+
+print(f"Execution Time: {execution_time:.6f} Milliseconds")
